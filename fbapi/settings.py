2dc50c1a02851c4979c11b66ec6d774874a12111
baljeet651 <technobaljeet651@gmail.com>
2020-04-22 16:12:58 +0530

    access token add

--- fbapi/settings.py
+++ fbapi/settings.py
@@ -148,17 +148,6 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'token',
