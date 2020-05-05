907007be87b94897b6a02a6c586e4b07e68c233d
baljeet651 <technobaljeet651@gmail.com>
2020-05-05 19:40:29 +0530

    additional files and changes for Heroku

--- fbapi/settings.py
+++ fbapi/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'fbapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'admin_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin@123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
