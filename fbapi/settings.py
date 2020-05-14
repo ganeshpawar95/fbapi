62a384bbd7c3862e7ee8be3e7393c7eb4d44bc00
baljeet651 <technobaljeet651@gmail.com>
2020-05-14 17:00:15 +0530

    impliment

--- fbapi/settings.py
+++ fbapi/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'fbapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'admin_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin@123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'admin_db',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin@123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 db_from_env = dj_database_url.config()
