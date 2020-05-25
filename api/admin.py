b428ae2e34a7b5093a35d01ff11c4056abf7d25e
baljeet651 <technobaljeet651@gmail.com>
2020-05-25 16:12:27 +0530

    Add model

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Post, Adset
+from .models import CustomUser, Post, Adset, AdsetOrignal
 
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Post)
 admin.site.register(Adset)
+admin.site.register(AdsetOrignal)
